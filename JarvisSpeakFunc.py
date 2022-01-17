f9e16b23d41e46a50534edeeaaa58939244efa3f
CodeWithAnand <96472106+AnandSinghSolanki@users.noreply.github.com>
2022-01-17 13:01:57 +0530

    Add files via upload

---
+++ JarvisSpeakFunc.py
@@ -0,0 +1,11 @@
+import pyttsx3 # pip install pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak("Subscribe to CodeWithAnand")
