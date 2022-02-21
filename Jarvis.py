1ae64c64b347fc68d37051fdccfcd5073706acbc
CodeWithAnand <96472106+Code-With-Anand@users.noreply.github.com>
2022-02-21 14:23:07 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,4 +1,5 @@
 import pyttsx3 # pip install pyttsx3
+import speech_recognition as sr # pip install SpeechRecognition
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -8,4 +9,23 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("Subscribe to CodeWithAnand")
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source,0,5)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'User Said: {query}\n')
+
+    except Exception as e:
+        print('Say that again please...')
+        return 'None'
+    return query.lower()
+
+if __name__ == "__main__":
+    while True:
+        query = listen().lower()
